128748145b7a01de01ae234e454fb7fb611491bf
sv.lubinskiy <sv.lubinskiy@tensor.ru>
2016-02-24 15:38:48 +0300

    public add

---
+++ public/assets/js/default_libs_script.min.js
@@ -0,0 +1,25 @@
+
+var app = {};
+
+(function(){
+
+
+	app = {
+
+		View: {},
+		Model: {},
+		Collection: {},
+		Router: {},
+
+		// заержка для сортировки
+		delay: 1000,
+
+		// классы дополнительные
+		sortClass: 'sort-class',
+		sortCompletedClass: 'sort-completed',
+		cloneClass: 'clone',
+
+	};
+
+
+})();
